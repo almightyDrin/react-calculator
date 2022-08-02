c71027585a189c9f0f0bebf506ed998108e10926
drin.dagsaan <aldrin.dagsaan@gmail.com>
2022-08-02 12:01:23 +0800

    Inintial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+React Calculator
+
+```
+src\
+ |--components\         # Display/Dumb components
+ |--context\            # Context & State stuff
+ |--hooks\              # Business logic/Smart components & Stateful things
+```
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from './components/ButtonBox'
+import Calculate from './hooks/useCalculate'
+import CalcProvider from "./context/CalcContext";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+function App() {
+  return (
+    <CalcProvider>
+      <Wrapper>
+        <Screen />
+        <ButtonBox>
+          {btnValues.flat().map((btn, i) => (
+            <Calculate
+              value={btn}
+              key={i}
+            />
+          ))}
+        </ButtonBox>
+      </Wrapper>
+    </CalcProvider>
+  );
+}
+
+export default App;
